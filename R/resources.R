ba43210268ed3b9adcc6097556defa8504fa8497
Christian Brueffer <christian.brueffer@med.lu.se>
2019-10-01 19:10:21 +0200

    Improve treatment labels.

--- R/resources.R
+++ R/resources.R
@@ -13,14 +13,14 @@ treatment.tbl <- as_tibble(rbind(c("any", "Any Treatment", "Any Treatment"),
                                  c("Adjuvant_Endo_only", "Endocrine only", "Endo only"),
                                  c("Adjuvant_Cyto_only", "Chemo only", "Chemo only"),
                                  c("Adjuvant_HER2_only", "HER2 only", "HER2 only"),
-                                 c("Adjuvant_Endo_and_Cyto", "Endo + Chemo + any", "Endo + chemo + any"),
+                                 c("Adjuvant_Endo_and_Cyto", "Endo + Chemo + any", "Endo + Chemo + any"),
                                  c("Adjuvant_Endo_and_HER2", "Endo + HER2 + any", "Endo + HER2 + any"),
                                  c("Adjuvant_Cyto_and_HER2", "Cyto + HER2 + any", "Chemo + HER2 + any"),
-                                 c("Adjuvant_Endo_and_Cyto_only", "Endo + Chemo only", "Endo + chemo only"),
+                                 c("Adjuvant_Endo_and_Cyto_only", "Endo + Chemo only", "Endo + Chemo only"),
                                  c("Adjuvant_Endo_and_HER2_only", "Endo + HER2 only", "Endo + HER2 only"),
                                  c("Adjuvant_Cyto_and_HER2_only", "Cyto + HER2 only", "Chemo + HER2 only"),
                                  c("Adjuvant_Endo_and_Cyto_and_HER2_only", "Endo + Chemo + HER2", "Endo + Chemo + HER2"),
-                                 c("Adjuvant_No_Systemic", "No Systemic Treatment", "Not Systemically")
+                                 c("Adjuvant_No_Systemic", "No Systemic Treatment", "Not Systemically Treated")
 ))
 colnames(treatment.tbl) <- c("var", "label", "plot.label")
 
